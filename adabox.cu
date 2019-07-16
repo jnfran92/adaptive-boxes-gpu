184f127b8ab1fde9f4dc798bea0e0e96bcffbd8c
Juan Chango <jchango@udlanet.ec>
2019-07-15 19:04:48 -0500

    working getters in device and host

--- adabox.cu
+++ adabox.cu
@@ -2,15 +2,53 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+// getters
+#include "./include/getters.h"
+
 //data
 #include "./data/squares.h"
 
 
+__device__ int sum_numbers(int a, int b){
+	return a+b;
+}
 
-void get_right_bottom_rectangle(int idx_i_arg, int idx_j_arg, long m_arg, long n_arg, int *data_matrix_arg);
-void get_left_bottom_rectangle(int idx_i_arg, int idx_j_arg, long m_arg, long n_arg, int *data_matrix_arg);
-void get_left_top_rectangle(int idx_i_arg, int idx_j_arg, long n_arg, int *data_matrix_arg);
-void get_right_top_rectangle(int idx_i_arg, int idx_j_arg, long n_arg, int *data_matrix_arg);
+__global__ void find_largest_rectangle(long m, long n, int *data_matrix){
+
+	const int coords_m = 5;
+	const int coords_n = 4;
+
+	__shared__ int coords[coords_m * coords_n];
+
+	int i = threadIdx.y;
+	int j = threadIdx.x;
+
+
+	int g_i = blockDim.y * blockIdx.y + i;
+	int g_j = blockDim.x * blockIdx.x + j;
+
+
+	printf("i %d  j %d   gi %d   gj %d \n",i,j,g_i,g_j);
+
+	if (i==0){
+		get_right_bottom_rectangle(0, 4, m, n, data_matrix);
+	}
+
+	if (i==1){
+		get_right_top_rectangle(0, 4, n, data_matrix);
+	}
+
+	if (i==2){
+		get_left_bottom_rectangle(0, 4, m, n, data_matrix);
+	}
+
+	if (i==3){
+		get_left_top_rectangle(0, 4, n, data_matrix);
+	}
+
+	/*coords[i*coords_n + 0] = */
+}
+
 
 int main(){
 	printf("adaptive-boxes-gpu\n");
@@ -24,277 +62,23 @@ int main(){
 	get_left_top_rectangle(0, 4, n, data);
 	get_right_top_rectangle(0, 4, n, data);
 
+	// CUDA
+	int *data_d;
+
+	// Get Mem
+	cudaMalloc((void **)&data_d, sizeof(int)*m*n );
+
+	// Copy to device
+	cudaMemcpy(data_d, data, sizeof(int)*m*n, cudaMemcpyHostToDevice);
 
 
-
+	dim3 grid(50, 50, 1);
+	dim3 block(1, 4, 1); // fixed size
+	
+	
+	find_largest_rectangle<<<grid, block>>>(m, n, data_d);
+	cudaDeviceSynchronize();
 
 
 	return 0;
 }
-
-
-void get_right_bottom_rectangle(int idx_i_arg, int idx_j_arg, long m_arg, long n_arg, int *data_matrix_arg){
-
-	int step_j = 0;
-	int first_step_i = 0;
-
-	int i_val = 0;
-	int j_val = 0;
-	int temp_val = 0;
-	int step_i = 0;
-	
-	int x1_val = 0;
-	int x2_val = 0;
-	int y1_val = 0;
-	int y2_val = 0;
-
-
-	while (true){
-		i_val = idx_i_arg;
-		j_val = idx_j_arg + step_j;
-
-		if(j_val == n_arg){
-			break;
-		}
-
-		temp_val = data_matrix_arg[i_val * n_arg + j_val];
-		if (temp_val == 0){
-			break;
-		}
-
-		step_i = 0;
-
-		while (true){
-			i_val = idx_i_arg + step_i;
-
-			if (i_val == m_arg){
-				break;
-			}
-
-			temp_val = data_matrix_arg[i_val * n_arg + j_val];
-
-			if(temp_val == 0){
-				break;
-			}
-			step_i++;
-		}
-
-
-		if (step_j == 0){
-			first_step_i = step_i;
-		}else{
-			if(step_i < first_step_i){
-				break;
-			}
-		}
-		step_j++;
-	}
-
-	x1_val = idx_j_arg;
-	y1_val = idx_i_arg;
-	x2_val = idx_j_arg + step_j - 1;
-	y2_val = idx_i_arg + first_step_i - 1;
-
-	printf("x1 %d   x2 %d    y1 %d    y2 %d\n", x1_val, x2_val, y1_val, y2_val);
-
-}
-
-
-void get_left_bottom_rectangle(int idx_i_arg, int idx_j_arg, long m_arg, long n_arg, int *data_matrix_arg){
-
-	int step_j = 0;
-	int first_step_i = 0;
-
-	int i_val = 0;
-	int j_val = 0;
-	int temp_val = 0;
-	int step_i = 0;
-	
-	int x1_val = 0;
-	int x2_val = 0;
-	int y1_val = 0;
-	int y2_val = 0;
-
-
-	while (true){
-		i_val = idx_i_arg;
-		j_val = idx_j_arg - step_j;
-
-		if(j_val == -1){
-			break;
-		}
-
-		temp_val = data_matrix_arg[i_val * n_arg + j_val];
-		if (temp_val == 0){
-			break;
-		}
-
-		step_i = 0;
-		while (true){
-			i_val = idx_i_arg + step_i;
-
-			if (i_val == m_arg){
-				break;
-			}
-
-			temp_val = data_matrix_arg[i_val * n_arg + j_val];
-
-			if(temp_val == 0){
-				break;
-			}
-			step_i++;
-		}
-
-
-		if (step_j == 0){
-			first_step_i = step_i;
-		}else{
-			if(step_i < first_step_i){
-				break;
-			}
-		}
-		step_j++;
-	}
-
-	x1_val = idx_j_arg;
-	y1_val = idx_i_arg;
-	x2_val = idx_j_arg - step_j + 1;
-	y2_val = idx_i_arg + first_step_i - 1;
-
-	printf("x1 %d   x2 %d    y1 %d    y2 %d\n", x1_val, x2_val, y1_val, y2_val);
-
-}
-
-
-void get_left_top_rectangle(int idx_i_arg, int idx_j_arg, long n_arg, int *data_matrix_arg){
-
-	int step_j = 0;
-	int first_step_i = 0;
-
-	int i_val = 0;
-	int j_val = 0;
-	int temp_val = 0;
-	int step_i = 0;
-	
-	int x1_val = 0;
-	int x2_val = 0;
-	int y1_val = 0;
-	int y2_val = 0;
-
-
-	while (true){
-		i_val = idx_i_arg;
-		j_val = idx_j_arg - step_j;
-
-		if(j_val == -1){
-			break;
-		}
-
-		temp_val = data_matrix_arg[i_val * n_arg + j_val];
-		if (temp_val == 0){
-			break;
-		}
-
-		step_i = 0;
-		while (true){
-			i_val = idx_i_arg - step_i;
-
-			if (i_val == -1){
-				break;
-			}
-
-			temp_val = data_matrix_arg[i_val * n_arg + j_val];
-
-			if(temp_val == 0){
-				break;
-			}
-			step_i++;
-		}
-
-
-		if (step_j == 0){
-			first_step_i = step_i;
-		}else{
-			if(step_i < first_step_i){
-				break;
-			}
-		}
-		step_j++;
-	}
-
-	x1_val = idx_j_arg;
-	y1_val = idx_i_arg;
-	x2_val = idx_j_arg - step_j + 1;
-	y2_val = idx_i_arg - first_step_i + 1;
-
-	printf("x1 %d   x2 %d    y1 %d    y2 %d\n", x1_val, x2_val, y1_val, y2_val);
-
-}
-
-
-void get_right_top_rectangle(int idx_i_arg, int idx_j_arg, long n_arg, int *data_matrix_arg){
-
-	int step_j = 0;
-	int first_step_i = 0;
-
-	int i_val = 0;
-	int j_val = 0;
-	int temp_val = 0;
-	int step_i = 0;
-	
-	int x1_val = 0;
-	int x2_val = 0;
-	int y1_val = 0;
-	int y2_val = 0;
-
-
-	while (true){
-		i_val = idx_i_arg;
-		j_val = idx_j_arg + step_j;
-
-		if(j_val == n_arg){
-			break;
-		}
-
-		temp_val = data_matrix_arg[i_val * n_arg + j_val];
-		if (temp_val == 0){
-			break;
-		}
-
-		step_i = 0;
-
-		while (true){
-			i_val = idx_i_arg - step_i;
-
-			if (i_val == -1){
-				break;
-			}
-
-			temp_val = data_matrix_arg[i_val * n_arg + j_val];
-
-			if(temp_val == 0){
-				break;
-			}
-			step_i++;
-		}
-
-
-		if (step_j == 0){
-			first_step_i = step_i;
-		}else{
-			if(step_i < first_step_i){
-				break;
-			}
-		}
-		step_j++;
-	}
-
-	x1_val = idx_j_arg;
-	y1_val = idx_i_arg;
-	x2_val = idx_j_arg + step_j - 1;
-	y2_val = idx_i_arg - first_step_i + 1;
-
-	printf("x1 %d   x2 %d    y1 %d    y2 %d\n", x1_val, x2_val, y1_val, y2_val);
-
-}
-
