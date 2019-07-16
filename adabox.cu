dc42f615fdf90cc30a1868248e23c754b5a9af4d
Juan Chango <jchango@udlanet.ec>
2019-07-16 15:13:18 -0500

    commit before addded args pointer array in kernel

--- adabox.cu
+++ adabox.cu
@@ -1,12 +1,16 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include <cooperative_groups.h>
 
 // getters
 #include "./include/getters.h"
 
 //data
-#include "./data/squares.h"
+#include "./data/complex.h"
+
+//Cooperative groups
+using namespace cooperative_groups;
 
 // GPU kernel
 __global__ void find_largest_rectangle(int idx_i, int idx_j, long m, long n, int *data_matrix, int *areas){
@@ -15,106 +19,133 @@ __global__ void find_largest_rectangle(int idx_i, int idx_j, long m, long n, int
 	const int coords_n = 4;
 
 	__shared__ int coords[coords_m * coords_n];
+	__shared__ int total_max;
 
 	int i = threadIdx.y;
 	int j = threadIdx.x;
 
 
-	int g_i = blockDim.y * blockIdx.y + i;
-	int g_j = blockDim.x * blockIdx.x + j;
+	/*int g_i = blockDim.y * blockIdx.y + i;*/
+	/*int g_j = blockDim.x * blockIdx.x + j;*/
 
 	int b_i = blockIdx.y;
 	int b_j = blockIdx.x;
-	int b_n = blockDim.y;
+	int b_n = gridDim.x;
 
 
 	/*printf("i %d  j %d   gi %d   gj %d \n",i,j,g_i,g_j);*/
 	
 	int results[4] = {};
 
-	if (i==0){
-		get_right_bottom_rectangle(idx_i, idx_j, m, n, data_matrix, results);
+	if (j==0){
+		get_right_bottom_rectangle(idx_i + b_i, idx_j+ b_j, m, n, data_matrix, results);
 	}
 
-	if (i==1){
-		get_right_top_rectangle(idx_i, idx_j, n, data_matrix, results);
+	if (j==1){
+		get_right_top_rectangle(idx_i + b_i, idx_j+ b_j, n, data_matrix, results);
 	}
 
-	if (i==2){
-		get_left_bottom_rectangle(idx_i, idx_j, m, n, data_matrix, results);
+	if (j==2){
+		get_left_bottom_rectangle(idx_i + b_i, idx_j+ b_j, m, n, data_matrix, results);
 	}
 
-	if (i==3){
-		get_left_top_rectangle(idx_i, idx_j, n, data_matrix, results);
+	if (j==3){
+		get_left_top_rectangle(idx_i + b_i, idx_j+ b_j, n, data_matrix, results);
 	}
 
 	/*printf("x1 %d    x2 %d   y1 %d   y2 %d   \n", results[0],  results[1],  results[2],  results[3]);*/
-	coords[i*coords_n + 0] = results[0];
-	coords[i*coords_n + 1] = results[1];
-	coords[i*coords_n + 2] = results[2];
-	coords[i*coords_n + 3] = results[3];
+	coords[j*coords_n + 0] = results[0];
+	coords[j*coords_n + 1] = results[1];
+	coords[j*coords_n + 2] = results[2];
+	coords[j*coords_n + 3] = results[3];
 
 	__syncthreads();
 
 
-	if (i==0){
+	if (j==0){
 		int a = coords[2*coords_n + 1];
 		int b = coords[3*coords_n + 1];
 		int pl = a;
 		if (b > a){
 			pl = b;
 		}
-		coords[4*coords_n + i] = pl;
+		coords[4*coords_n + j] = pl;
 	}
 
-	if (i==1){
+	if (j==1){
 		int a = coords[0*coords_n + 1];
 		int b = coords[1*coords_n + 1];
 		int pr = a;
 		if (b < a){
 			pr = b;
 		}
-		coords[4*coords_n + i] = pr;
+		coords[4*coords_n + j] = pr;
 	}
 
-	if (i==2){
+	if (j==2){
 		int a = coords[1*coords_n + 3];
 		int b = coords[3*coords_n + 3];
 		int pt = a;
 		if (b > a){
 			pt = b;
 		}
-		coords[4*coords_n + i] = pt;
+		coords[4*coords_n + j] = pt;
 	}
 
-	if (i==3){
+	if (j==3){
 		int a = coords[0*coords_n + 3];
 		int b = coords[2*coords_n + 3];
 		int pb = a;
 		if (b < a){
 			pb = b;
 		}
-		coords[4*coords_n + i] = pb;
+		coords[4*coords_n + j] = pb;
 	}
 
 	__syncthreads();
 
-	printf("final x1 %d,    x2 %d,    y1 %d,    y2 %d \n",coords[4*coords_n + 0], coords[4*coords_n + 1],coords[4*coords_n + 2] ,coords[4*coords_n + 3]);
+	/*printf("final x1 %d,    x2 %d,    y1 %d,    y2 %d \n",coords[4*coords_n + 0], coords[4*coords_n + 1],coords[4*coords_n + 2] ,coords[4*coords_n + 3]);*/
 
 	// get area
-	if (i==0){
+	if (j==0){
 		int a = abs( coords[coords_n*4 + 0] -  coords[coords_n*4 + 1] );
 		int b = abs( coords[coords_n*4 + 2] -  coords[coords_n*4 + 3] );
 		int area = a*b;
 		printf("area %d\n", area);
 	 	coords[coords_n*0 + 0] = area; // Saving area in coords[0][0]
 	       	areas[b_i*b_n + b_j] = area;
-		printf("bi %d    bj %d\n", b_i,b_j);
+		/*printf("bi %d    bj %d     bn %d\n", b_i,b_j,b_n);*/
 	}
 	__syncthreads();
 
 	// get max area all blocks
+	if (b_j == 0){
+		int temp_area = areas[b_i*b_n + j];
+		atomicMax(&total_max, temp_area);
+		__syncthreads();
+		if(j == 0){
+			/*areas[b_i*b_n + 0] = total_max;			*/
+			atomicMax(&areas[0*b_n + 0], total_max);
+			printf("total_max %d -  bi  %d \n", areas[0*b_n + 0], b_i);
+		}
+	}
 
+	__syncthreads();
+
+	grid_group grid = this_grid();
+	grid.sync();
+
+	// get final x1 x2 y1 y2
+	if (j == 0){
+		int a = coords[coords_n*0 + 0];
+		int b = areas[b_n*0 + 0];
+		/*printf("a %d, b%d\n",a,b);*/
+
+		if(a==b){
+			printf("final x1 %d,    x2 %d,    y1 %d,    y2 %d \n",coords[4*coords_n + 0], coords[4*coords_n + 1],coords[4*coords_n + 2] ,coords[4*coords_n + 3]);
+		}
+	}
+	
 
 }
 
@@ -126,6 +157,19 @@ int main(){
 	printf("m %ld , n% ld\n",m, n);
 	
 	/*int out[4] = {};	*/
+
+	int a = 1;
+	long b = 2;
+	int c[3] = {1,2,3};
+	void *args[] = {&a, &b, c};
+
+	int *cc;
+	cc = (int *)args[2];
+
+	printf("args [1]%d  [2]%ld [3]%d \n", *(int *)args[0], *(long *)args[1], cc[2]);
+	
+	
+
 
 	/*get_right_bottom_rectangle(8, 10, m, n, data, out);*/
 	/*printf("--->x1 %d    x2 %d   y1 %d   y2 %d   \n", out[0],  out[1],  out[2],  out[3]);*/
@@ -140,8 +184,8 @@ int main(){
 
 	// CUDA
 	//    number of tests = grid_x*grid_y	
-	int grid_x = 2;
-	int grid_y = 2;
+	int grid_x = 4; // fxed
+	int grid_y = 1; //
 	
 	int *data_d;
 	int *areas;
@@ -154,11 +198,12 @@ int main(){
 	cudaMemcpy(data_d, data, sizeof(int)*m*n, cudaMemcpyHostToDevice);
 
 	dim3 grid(grid_x, grid_y, 1);
-	dim3 block(1, 4, 1); // fixed size
+	dim3 block(4, 1, 1); // fixed size
 	
-	int idx_i = 10;
-	int idx_j = 15;	
+	int idx_i = 100;
+	int idx_j = 150;	
 
+	void *kernel_args[] = {&idx_i, &idx_j, &m, &n, data_d, areas};
 	find_largest_rectangle<<<grid, block>>>(idx_i, idx_j, m, n, data_d, areas);
 	cudaDeviceSynchronize();
 
