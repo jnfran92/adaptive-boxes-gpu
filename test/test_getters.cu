cf43d565a8823ba29506d267cc7e89da4a385c5b
Juan Chango <jchango@udlanet.ec>
2019-07-29 14:20:56 -0500

    bugs fixed using test

--- test/test_getters.cu
+++ test/test_getters.cu
@@ -8,8 +8,10 @@
 #include "../include/getters.h"
 #include "../include/getters_improved.h"
 // data
-#include "../data/squares.h"
+#include "../data/hall10.h"
 
+
+bool verify_results(int *expected, int *actual, bool print);
 
 int main(int argc, char *argv[]){
 	printf("adaptive-boxes-gpu\n");
@@ -18,42 +20,166 @@ int main(int argc, char *argv[]){
 	printf("----> Data size: m %ld , n% ld\n",m, n);
 
 
+	int out_expected[4] = {};
 	int out[4] = {};
 
 	int point_to_test_i;
 	int point_to_test_j;
 
 	// Test
-	point_to_test_i = 5;
-	point_to_test_j = 5;
-	
-	if (data[point_to_test_i*n + point_to_test_j] == 1){
-		printf("is valid point\n");
+	for (int idx_j=0; idx_j<n; idx_j++){
+		
+		for (int idx_i=0; idx_i<m; idx_i++){
+		
+			point_to_test_i = 0 + idx_i;
+			point_to_test_j = 0 + idx_j;
+			
+			if (data[point_to_test_i*n + point_to_test_j] == 1){
+				printf("Testing in i %d j %d ---------------\n",
+						point_to_test_i
+						,point_to_test_j);
+
+
+				bool is_ok;
+				
+				printf("Test RB\n");
+				// old getters
+				og::get_right_bottom_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						m,
+						n,
+						data,
+						out_expected
+						);
+
+				// new getters
+				ng::get_right_bottom_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						m,
+						n,
+						data,
+						out
+						);
+				
+				is_ok = verify_results(out_expected, out, true);
+				printf("\n");
+
+				
+				printf("Test LB\n");
+				// old getters
+				og::get_left_bottom_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						m,
+						n,
+						data,
+						out_expected
+						);
+
+				// new getters
+				ng::get_left_bottom_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						m,
+						n,
+						data,
+						out
+						);
+				
+				is_ok = verify_results(out_expected, out, true);
+				printf("\n");
+
+
+				printf("Test LT\n");
+				// old getters
+				og::get_left_top_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						n,
+						data,
+						out_expected
+						);
+
+				// new getters
+				ng::get_left_top_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						n,
+						data,
+						out
+						);
+				
+				is_ok = verify_results(out_expected, out, true);
+				printf("\n");
+
+				
+				printf("Test RT\n");
+				// old getters
+				og::get_right_top_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						n,
+						data,
+						out_expected
+						);
+
+				// new getters
+				ng::get_right_top_rectangle(
+						point_to_test_i,
+						point_to_test_j,
+						n,
+						data,
+						out
+						);
+				
+				is_ok = verify_results(out_expected, out, true);
+				printf("\n");
+			}
+		}
 	}
-
-	// old getters
-	og::get_right_bottom_rectangle(
-			point_to_test_i,
-			point_to_test_j,
-			m,
-			n,
-			data,
-			out
-			);
-	printf("--->x1 %d    x2 %d   y1 %d   y2 %d   \n", out[0],  out[1],  out[2],  out[3]);
-
-	// new getters
-	ng::get_right_bottom_rectangle(
-			point_to_test_i,
-			point_to_test_j,
-			m,
-			n,
-			data,
-			out
-			);
-	printf("--->x1 %d    x2 %d   y1 %d   y2 %d   \n", out[0],  out[1],  out[2],  out[3]);
-
-
 
 	return 0;
 }
+
+
+bool verify_results(int *expected, int *actual, bool print){
+
+
+
+	bool test_passed = false;
+	if ( (expected[0] != actual[0]) ||
+		        (expected[1] != actual[1]) ||
+ 			(expected[2] != actual[2]) ||
+ 			(expected[3] != actual[3]) ){
+		if (print)
+		printf("Test Failed\n");
+
+		if (print)
+		printf("EXPECTED: x1 %d    x2 %d   y1 %d   y2 %d   \n",
+				expected[0],
+				expected[1], 
+				expected[2], 
+				expected[3]);
+
+
+		if (print)
+		printf("ACTUAL: x1 %d    x2 %d   y1 %d   y2 %d   \n", 
+				actual[0],  
+				actual[1],  
+				actual[2],  
+				actual[3]);
+
+	}else{
+		if (print)
+		printf("Test Passed\n");
+		test_passed = true;
+	}
+
+	return test_passed;
+}
+
+
+
+
